43c2e86cde609f0115b14c3876a6570549609352
ranjangrg <32595621+ranjangrg@users.noreply.github.com>
2021-07-30 23:26:00 +0100

    First commit

---
+++ lib/darkmode/code.ts
@@ -0,0 +1,49 @@
+/*
+	Main DarkModeApp class definition and implementation
+*/
+
+export class DarkModeApp {
+	constructor() {
+		// console.log("Dark mode object created ...");
+		// maybe call this.init() here!??
+	}
+	init() {
+		this._init_dark_mode_btn();
+		this._init_dark_mode_features();
+		this._init_color_mode_within_dom();
+	}
+	_init_dark_mode_btn() {
+		let toggler_elem: HTMLElement = document.getElementById("mode-toggler")!;
+		toggler_elem.innerHTML = 
+		`<label class="switch">
+			<input id="mode-btn" type="checkbox">
+			<span class="slider round"></span>
+		</label>`;
+	}
+	_init_dark_mode_features() {
+		const mode_btn = document.getElementById("mode-btn")! as HTMLInputElement;
+	
+		mode_btn.onchange = (e) => {
+			let curr_mode: string = "dark-mode";
+			if (mode_btn.checked) {
+				curr_mode = "light-mode";
+			} 
+			let all_elems = document.getElementsByClassName(curr_mode) as HTMLCollection;
+			for (let elem of [...all_elems]) {
+				elem.classList.toggle("light-mode");
+				elem.classList.toggle("dark-mode");
+			};
+		};
+	}
+	
+	// adds 'light-mode' class to all [data-dm-use-color-mode]
+	// within [data-dm-enable-color-mode]
+	_init_color_mode_within_dom() {
+		// needs [data-dm-enable-color-mode] to be set first
+		let node_list: NodeListOf<HTMLElement> = document.querySelectorAll("[data-dm-enable-color-mode] [data-dm-use-color-mode]");
+		node_list.forEach( (nd) => {
+			nd.classList.toggle("light-mode");
+		} );
+	}
+}
+
